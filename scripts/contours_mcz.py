f6680a7421f94fb183d3bef49138e04d505750d7
fairytien <fairynguyen33@gmail.com>
2024-07-23 19:59:13 -0500

    Save pickled `sys3_mismatch_NP_L` data; Create `contours_mcz.py` script to make RP vs L mismatch data over `mcz`

---
+++ scripts/contours_mcz.py
@@ -0,0 +1,33 @@
+import sys, os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from modules.contours_ver2 import *
+
+
+@timer_decorator
+def main():
+    print("Number of cores: ", cpu_count())
+    results = {}
+    # Assign parameters
+    mcz_arr = np.linspace(10, 90, 250)
+
+    for mcz in mcz_arr:
+        lens_params, RP_params = set_to_location(
+            loc_params["Taman"]["random"], lens_params_1, RP_params_1
+        )
+        lens_params["mcz"] = RP_params["mcz"] = mcz * solar_mass
+        I = 0.5
+        td = 0.03
+        y = get_y_from_I(I)
+        MLz = get_MLz_from_td(td, y)
+        lens_params["y"] = y
+        lens_params["MLz"] = MLz * solar_mass
+        print(f"{mcz:.3g}")  # For tracking progress
+
+        results[mcz] = mismatch_contour_parallel(RP_params, lens_params)
+
+    filepath = pickle_data(results, "data", "sys3_contours_mcz" + str(mcz))
+
+
+if __name__ == "__main__":
+    main()
