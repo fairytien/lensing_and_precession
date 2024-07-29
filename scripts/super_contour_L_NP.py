163ad537fa7745fd8ed9d9e3da573ceb0e13bcd3
Tien Nguyen <fairynguyen33@gmail.com>
2024-07-29 10:30:39 -0500

    Create super contour for L vs NP for `mcz=40`

--- scripts/super_contour_L_NP.py
+++ scripts/super_contour_L_NP.py
@@ -12,7 +12,7 @@ def main():
     lens_params, RP_params, NP_params = set_to_location(
         loc_params["Taman"]["random"], lens_params_1, RP_params_1, NP_params_1
     )
-    mcz = 20
+    mcz = 40
     lens_params["mcz"] = RP_params["mcz"] = NP_params["mcz"] = mcz * solar_mass
     # RP_params["omega_tilde"] = 3
     # limits = get_lens_limits_for_RP_L(RP_params, lower=0.5)
@@ -26,7 +26,7 @@ def main():
         NP_params, lens_params, td_arr, I_arr, what_template="NP"
     )
 
-    filepath = pickle_data(results, "data", "super_contour_NP_L_mcz" + str(mcz))
+    filepath = pickle_data(results, "data", "sys3_super_contour_L_NP_mcz" + str(mcz))
 
 
 if __name__ == "__main__":
