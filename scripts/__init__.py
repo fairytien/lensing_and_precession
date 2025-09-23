110fe1546e3b2f6c0220f0eb6c992d23c1d8d9f0
fairytien <fairynguyen33@gmail.com>
2025-09-23 19:29:37 -0400

    added to scripts/

---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+# The __init__.py file is a special file in Python. It's often empty, but it can also contain Python code. Its main roles are:
+
+# Indicate that a directory is a Python package, so it can be imported the same way a module can be imported.
+
+# Define what should happen when a package is imported. If there's Python code in the __init__.py file, it will be run when the package is imported.
+
+# Define a specific API for the package. You can choose to import specific modules when the package is imported, or define specific attributes.
