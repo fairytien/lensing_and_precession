ef4262dff6471f8b7e3f00fc16f335858fc6f1ce
fairytien <fairynguyen33@gmail.com>
2026-03-03 04:57:17 -0500

    moved files

--- scripts/super_contours/super_contour_L_NP.py
+++ scripts/super_contours/super_contour_L_NP.py
@@ -1,6 +1,8 @@
 import sys, os
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.insert(
+    0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+)
 from modules.contours_v2 import *
 
 
